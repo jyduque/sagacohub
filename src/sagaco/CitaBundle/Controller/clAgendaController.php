5e1eec536dff3e1254892a717aee3c44b46e7ffd
Jhanmara Duque <jyduque@unet.edu.ve>
2015-03-09 20:14:46 -0430

    Agregar nueva clase clAgendaController desde CitaBundle Signed-off-by:Jhanmara Duque <jyduque@unet.edu.ve>

---
+++ src/sagaco/CitaBundle/Controller/clAgendaController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace sagaco\CitaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+//use sagaco\DsagacoBundle\Entity\clAgendaOrientador;
+//use sagaco\DsagacoBundle\Entity\clAgendaOrientadorRepository;
+//use sagaco\CitaBundle\Form\Extranet\clAgendaOrientadorType;
+
+
+/**
+ * Controlador de clAgendaOrientador.
+ *
+ * @Route("/pgAgenda")
+ */
+class clAgendaController extends Controller
+{
+    var $blnBandera;
+    
+    /**
+     * Lists todas las entidades de clAgendaOrientador.
+     *
+     * @Route("/", name="pgAgenda")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        //$objEntidad = $em->getRepository('DsagacoBundle:clAgendaOrientador')->listar();  
+        
+        $intOrientador = '1';
+        $intSemestre = '1';
+        
+        
+        //$objEntidad = $em->getRepository('DsagacoBundle:clAgendaOrientador')->calendario($intOrientador, $intSemestre);        
+        $id=1;
+        $objEntidad = $em->getRepository('DsagacoBundle:clAgendaOrientador')->find($id);        
+        
+        
+        
+                
+       
+        //return $this->render('CitaBundle:clAgenda:index.html.twig',
+          //      array('objEntidad' => $objEntidad));
+        return ['objEntidad' => $objEntidad];
+    }  
+}
